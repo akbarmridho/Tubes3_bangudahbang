c6b47418a980601327fc7dc13c971a07abbd0e87
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-04-22 17:30:43 +0700

    feat: kmp implementation

--- src/backend/utils/string-matcher/kmp.go
+++ src/backend/utils/string-matcher/kmp.go
@@ -1,1 +1,47 @@
 package string_matcher
+
+func KMP(patternStr string, toMatchStr string) []int {
+	result := make([]int, 0)
+
+	// use rune to handle UTF-8 chars instead of ascii
+	pattern := []rune(patternStr)
+	toMatch := []rune(toMatchStr)
+	patternLen := len(pattern)
+
+	// compute border
+	borders := make([]int, patternLen)
+	j, i := 0, 1
+
+	for i < patternLen {
+		if pattern[j] == pattern[i] {
+			// j+1 chars match
+			borders[i] = j + 1
+			i++
+			j++
+		} else if j > 0 { // j follows matching prefix
+			j = borders[j-1]
+		} else { // no match
+			borders[i] = 0
+			i++
+		}
+	}
+
+	// begin matching
+	i, j = 0, 0
+
+	for i < len(toMatch) {
+		if pattern[j] == toMatch[i] {
+			if j == patternLen-1 {
+				result = append(result, i-patternLen+1) // found match
+			}
+			i++
+			j++
+		} else if j > 0 {
+			j = borders[j-1]
+		} else {
+			i++
+		}
+	}
+
+	return result
+}
