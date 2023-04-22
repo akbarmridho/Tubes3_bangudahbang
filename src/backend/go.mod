6dcf9dd43be81d16ff21d0942565e46bcfd62bdd
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-04-22 19:59:17 +0700

    feat: add session id to history as uuid

--- src/backend/go.mod
+++ src/backend/go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
--- src/backend/models/history.go
+++ src/backend/models/history.go
@@ -1,9 +1,13 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
+)
 
 type History struct {
 	gorm.Model
-	UserQuery string `json:"user_query"`
-	Response  string `json:"response"`
+	UserQuery string    `json:"user_query"`
+	Response  string    `json:"response"`
+	SessionId uuid.UUID `json:"session_id" gorm:"type:uuid;index"`
 }
