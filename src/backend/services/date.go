2e520cbe35c0ee5c1746adaee140f96986e40fc6
chaerla <chelgchen@gmail.com>
2023-04-26 04:21:37 +0800

    feat: implemented date

--- src/backend/services/date.go
+++ src/backend/services/date.go
@@ -1,5 +1,15 @@
 package services
 
+import (
+	"time"
+)
+
 func GetDay(input string) (string, error) {
-	return "Not implemented yet", nil
+	layout := "02/01/2006 15:04:05"
+	date := input + " 00:00:00"
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		return "I'm sorry I can't get the day of the date, the date inputted is invalid. Make sure it's DD/MM/YYYY", nil
+	}
+	return t.Weekday().String(), nil
 }
