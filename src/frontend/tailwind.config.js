ea4c28a26ea14f52db122e41d1fc940f71e06209
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2023-05-01 01:31:41 +0700

    set up themes, fonts

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        base: '#2977EF',
+        dark: '#1363DF',
+      },
+      secondary: {
+        base: '#111825',
+        dark: '#010101',
+      },
+    },
+    extend: {
+      colors: {
+        white: '#FBFEFF',
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
